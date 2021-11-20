83b446f8f96642aa8d71560bc63bb353839317ea
vssreddy423 <92976622+vssreddy423@users.noreply.github.com>
2021-11-20 14:11:26 +0530

    Update can.h

--- can.h
+++ can.h
@@ -21,21 +21,27 @@ uint8_t data[8];     //data field
 // Bypass mode acive
 #define CAN1_Bypass()             LPC_CANAF->AFMR=(2<<0)
 // Reset mode - Normal & Reset
-#define CAN1_Normal_RM()          LPC_CAN1->MOD=0
 #define CAN1_Reset_RM()           LPC_CAN1->MOD=(1<<0)
+#define CAN1_Normal_RM()          LPC_CAN1->MOD=0
 // Baud Rate Prescalar, time segment 1&2
 #define BRP   12
 #define TSEG1 12
 #define TSEG2  1
 #define CAN1_BTR()                LPC_CAN1->BTR=(TSEG2<<20)|(TSEG1<<16)|(BRP<<0)
 // Trasmit Buffer Status check to write in TFI
-#define CAN1_TBS_Status()         while(!(LPC_CAN1->SR &(1<<2)))
+#define CAN1_TBS_Status()         LPC_CAN1->SR &(1<<2)
 // Receive Status to read RFS
-#define CAN1_RS_Status()          while(!(LPC_CAN1->SR &(1<<4)))
-//  DLC to TFI
+#define CAN1_RS_Status()          while(LPC_CAN1->SR &(1<<4))
+//  Trasmission requirements
 #define CAN1_TFI(datalen)         LPC_CAN1->TFI1=datalen
-//  read RFS
+#define CAN1_Id(id)               LPC_CAN1->TID1=id
+#define CAN1_Data1(data)          LPC_CAN1->TDA1=data
+#define CAN1_Data2(data)          LPC_CAN1->TDB1=data
+//  Receiver requirements
 #define CAN1_RFS()                LPC_CAN1->RFS
+#define CAN1_RID()                LPC_CAN1->RID
+#define CAN1_RDA()                LPC_CAN1->RDA
+
 
 __inline static void Set_CAN1_Base_Config(void);
 __inline static void Set_CAN1_Core_Config(void);
